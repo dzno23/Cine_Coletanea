3b604e0abb03f9ce960615f96222d745e61d5697
Thiago Henrique <dzno23@gmail.com>
2022-04-28 23:18:12 -0300

    Correções no arquivo urls da pasta do projeto.
    Eu havia descrito o caminho da MEDIA_URL errado.

--- cinecoletanea/settings.py
+++ cinecoletanea/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- cinecoletanea/urls.py
+++ cinecoletanea/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
-    path('coletanea/', include('coletane.urls')),
-    
+    path('coletanea/', include('coletanea.urls')),
+
 ]
